a9f8bfeeaca879fb8162940d75f4b1cc3a2f9412
alimustafa905000-rgb <alimustafa905000@gmail.com>
2026-06-25 00:28:08 +0500

    Added Day 4 Quiz App project

---
+++ Day4-Quiz-App/README.md
@@ -0,0 +1,43 @@
+# Quiz App 
+
+A simple and interactive Quiz Application built using (HTML, CSS, and JavaScript).  
+This project is part of my 30 Days JavaScript Challenge (Day 4).
+
+# Features:
+
+- Multiple choice questions
+- Next button to move to the next question
+- Score tracking system
+- Highlights correct answers in green
+- Highlights wrong answers in red
+- Shows final score at the end
+- Restart quiz option
+
+# Technologies Used 
+
+- HTML5
+- CSS3
+- JavaScript (DOM Manipulation)
+
+# How It Works:
+
+1. The quiz starts automatically.
+2. One question is shown at a time.
+3. User selects an answer.
+4. Correct answer is highlighted.
+5. Score updates if the answer is correct.
+6. User clicks **Next** for the next question.
+7. At the end, the total score is displayed.
+8. User can restart the quiz.
+
+## JavaScript Concepts Used:
+
+- Arrays of Objects
+- Functions
+- Event Listeners
+- DOM Manipulation
+- Dynamic Button Creation
+- Conditional Statements
+- Loops
+- Dataset Attribute
+- State Management
---
+++ Day4-Quiz-App/script.js
@@ -0,0 +1,136 @@
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        answers: [
+            { text: "Hyper Text Markup Language", correct: true },
+            { text: "High Text Machine Language", correct: false },
+            { text: "Home Tool Markup Language", correct: false },
+            { text: "Hyper Tool Multi Language", correct: false }
+        ]
+    },
+    {
+        question: "Which language is used for styling web pages?",
+        answers: [
+            { text: "HTML", correct: false },
+            { text: "JQuery", correct: false },
+            { text: "CSS", correct: true },
+            { text: "XML", correct: false }
+        ]
+    },
+    {
+        question: "Which language is used for web interactivity?",
+        answers: [
+            { text: "Python", correct: false },
+            { text: "JavaScript", correct: true },
+            { text: "C++", correct: false },
+            { text: "Java", correct: false }
+        ]
+    },
+     {
+        question: "Which language is used for styling web pages?",
+        answers: [
+            { text: "HTML", correct: false },
+            { text: "JQuery", correct: false },
+            { text: "CSS", correct: true },
+            { text: "XML", correct: false }
+        ]
+    },
+    {
+        question: "What does SQL stand for?",
+        answers: [
+            { text: "Structured Query Language", correct: true },
+            { text: "Simple Query Language", correct: false },
+            { text: "Statement Query Language", correct: false },
+            { text: "System Query Language", correct: false }
+        ]
+    }
+];
+
+const questionElement = document.querySelector("#question");
+const answerButtons = document.querySelector("#answer-buttons");
+const nextButton = document.querySelector("#next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerText = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerText = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerText = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerText = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
+
+startQuiz();
